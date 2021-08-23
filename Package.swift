eaeae3b4a966383eb71fc0f057ce8b99ebc45254
Keith Smiley <keithbsmiley@gmail.com>
2021-08-22 21:10:27 -0700

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -4,10 +4,15 @@ import PackageDescription
 let package = Package(
     name: "fix-macos-internal-sdk",
     dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/tuist/XcodeProj.git", .upToNextMajor(from: "8.0.0")),
     ],
     targets: [
         .target(
             name: "fix-macos-internal-sdk",
-            dependencies: []),
+            dependencies: [
+                "XcodeProj",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
     ]
 )
