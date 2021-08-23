ce2beff4d51c6c8a7a2194a3bf2dd37e3ba3604e
Keith Smiley <keithbsmiley@gmail.com>
2021-08-22 21:37:10 -0700

    Fix executable name

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "fix-macos-internal-sdk",
+    name: "fix-macosx-internal-sdk",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/tuist/XcodeProj.git", .upToNextMajor(from: "8.0.0")),
     ],
     targets: [
         .target(
-            name: "fix-macos-internal-sdk",
+            name: "fix-macosx-internal-sdk",
             dependencies: [
                 "XcodeProj",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
